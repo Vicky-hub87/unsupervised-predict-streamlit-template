584fd34e8d7b731cf4c5e601a3ca898987c38e73
Lawrence <ltjatjie@gmail.com>
2020-07-20 13:34:59 +0200

    solution overview text

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -105,7 +105,45 @@ def main():
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
+        #st.write("Describe your winning approach on this page")
+
+        st.title('Movie Recommendation System Engine')
+        st.info('General Information') 
+        st.image(r'resources/imgs/unnamed.jpg',use_column_width=True)
+        st.header('**About Recommendation Systems **')
+        st.markdown('''Recommendation systems are becoming increasingly important in today’s extremely busy world. People are always short on time with the myriad tasks they need to accomplish in the limited 24 hours. Recommender systems are socially and economically critical for ensuring that individuals can make appropriate choices surrounding the content they engage with on a daily basis. Therefore, the recommendation systems are important as they help them make the right choices.
+                    The purpose of a recommendation system is to search for content that would be interesting to an individual. Moreover, it involves a number of factors to create personalised lists of useful and interesting content specific to each user.Another objective of the recommendation system is to achieve customer loyalty by providing relevant content and maximising the time spent by a user on your website or channel
+                    ''')
+        st.header(' **Why Movie Recommendation Systems **')
+        st.markdown(''' i. They help the user find movies of their interest.''')
+        st.markdown('''  ii. Helps the item provider to deliver their items to the right user.''')
+        st.markdown(''' iii.    Showcase personalised content to each user.''')
+        st.markdown(''' iv. Websites can improve user-engagement to the website''')
+        st.header(' **Movie Recommendation System Used**')
+        st.image(r'resources/imgs/Collaborative_filtering_vbujt7.gif',use_column_width=True)
+        st.markdown('''Recommendations are driven by Machine Learning Algorithms. This Recommendation Engine uses Collaborative Filtering and Content-Based Filtering to predict unseen movie ratings gathered from thousands of users based on the historical preference.''')
+        st.markdown('''When a movie website misses a recommendation system, it results in users browsing through a long list of movies, with no suggestions about what to watch. This, in turn, reduces the propensity of a user to engage with the website and use its services. This recommendation system will help users get recommendations of movies based on similar movies they once watched or based on what other users who have similar interest in movies as them have loved watching. ''')
+        st.image(r'resources/imgs/unnamed2.jpg',use_column_width=True)
+
+        st.header('**Collaborative Filtering**')
+        st.markdown('''i.   User-based Filtering: these systems recommend products to a user that similar users have liked. These systems are widely used, and they try to predict the rating or preference that a user would give an item-based on past ratings and preferences of other users.''')
+        st.markdown('''ii.  Item-based Filtering: These systems identify similar items based on how people have rated it in the past.''')
+        st.header('**Content-based Filtering**')
+        st.markdown('''This system uses item metadata, such as genre, director, description, actors, for movies, to make these recommendations. The general idea behind these recommender systems is that if a person likes a particular item, he or she will also like an item that is similar to it. And to recommend that, it will make use of the user's past item metadata.''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         #data
         #Movie data has movie ID movie name and genre(s)
@@ -137,7 +175,10 @@ def main():
                     st.markdown('''Top 10 rated movies and their Budget''')
                     st.image(image, use_column_width=True)
             elif st.checkbox('Overall'):
-                pass
+                st.markdown("""
+                    <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/224974a4-669e-4103-8052-66b7674d7c97/page/CndYB" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    """, unsafe_allow_html=True)
+                               
 
 
     # You may want to add more sections here for aspects such as an EDA,
